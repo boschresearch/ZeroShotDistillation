30f219616130d8a4ca4031b06b1ad7d97c74463d
Niclas Popp <150654529+NPROBO@users.noreply.github.com>
2025-02-14 10:44:38 +0100

    Create StudentArchitectureComponents.py

---
+++ utils/StudentArchitectureComponents.py
@@ -0,0 +1,56 @@
+"""
+    Classes for the student architecture components.
+"""
+import torch
+from torchvision import models
+from torch import nn
+from torch.nn import functional
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import TQDMProgressBar
+import torch.multiprocessing as mp
+import numpy as np
+from numpy.linalg import norm
+from torchmetrics import Accuracy
+import clip
+from nltk.corpus import wordnet as wn
+import timm
+import os
+import sys
+import random
+import time
+from uuid import uuid4
+from torch import autocast
+
+# Class is adapted from: https://github.com/moein-shariatnia/OpenAI-CLIP/blob/master/CLIP.py (visited 24/11/2023)
+class ImageEncoder(nn.Module):
+    """
+        Class for the image encoder.
+    """
+    def __init__(self,args):
+        super().__init__()
+        self.model = timm.create_model(args.model, False, num_classes=0, global_pool="avg")
+
+    def forward(self, x):
+        return self.model(x)
+
+# Class is taken from: https://github.com/moein-shariatnia/OpenAI-CLIP/blob/master/CLIP.py (visited 24/11/2023)
+class Projection(nn.Module):
+    """
+        Class for the single-layer projection head. Two-layer projection head with non-linearity is commented.
+    """
+    def __init__(self,args):
+        super().__init__()
+        self.projection = nn.Linear(args.encoder_output_dim, args.embedding_dim)
+        # self.relu = nn.ReLU()
+        # self.fc = nn.Linear(args.embedding_dim, args.embedding_dim)
+        # self.layer_norm = nn.LayerNorm(args.embedding_dim)
+    
+    def forward(self, x):
+        projected = self.projection(x)
+        # x = self.relu(projected)
+        # x = self.fc(x)
+        # x = x + projected
+        # x = self.layer_norm(x)
+        # return x
+        return projected
