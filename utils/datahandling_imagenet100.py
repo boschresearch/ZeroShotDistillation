378551b1fe292a9f90a25437b6474978de217d63
Niclas Popp <150654529+NPROBO@users.noreply.github.com>
2025-07-23 13:47:53 -0400

    Update datahandling_imagenet100.py

--- utils/datahandling_imagenet100.py
+++ utils/datahandling_imagenet100.py
@@ -55,7 +55,7 @@ def get_dataset(
                             Resize(224),
                             CenterCrop(224),
                             ToTensor(),
-                            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # ImageNet normalization here
                         ]
                     )
     else:
