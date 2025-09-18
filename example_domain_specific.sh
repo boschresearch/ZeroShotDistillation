b5fb3095da1e6d35c640648e98a15872d5c2f981
Niclas Popp <150654529+NPROBO@users.noreply.github.com>
2025-09-18 16:17:19 -0400

    Add headers and refactor

--- example_domain_specific.sh
+++ example_domain_specific.sh
@@ -1,22 +1,24 @@
 python train.py \
---train /aircraft \
---batch_size 8 \
+--train /data_train \
+--val /data_val \
+--batch_size 256 \
 --epochs 96 \
---teacher ViT-B/32 \
+--teacher ViT-B-32 \
 --model tiny_vit_11m_224 \
 --embedding_dim 512 \
 --encoder_output_dim 448 \
---dataset aircraft \
+--dataset pets \
 --logdir /logs \
+--logname pets_domain_specific \
 --synthetic_data True \
 --diverse_prompts True \
+--use_diverse_prompts_for_inference True \
 --options_per_attribute 15 \
---logname finetuning \
 --training_loss CLIP \
 --distillation_loss L2 \
---distil_alpha 0.5 \
+--distil_alpha 0.0 \
 --learning_rate 0.0005 \
 --weight_decay 0.0 \
 --devices 1 \
 --nodes 1 \
---start_model_path starting_checkpoint.ckpt # checkpoint from domain-agnostic training
+--start_model_path "/path/to/model.ckpt"
